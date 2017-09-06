48ec70380802cf44dd9a7dc282e899d4ced5a601
Tyler Braddock <tyler.braddock@yahoo.com>
2017-09-06 20:53:32 +0000

    Create layouts/_header.html.erb and implement general layout of header/top-navigation. Render partial from within application.html.erb. Add top-navigation styling in master.scss. Create top-nav.js. Write script for rotating weightlifter.png and fading in/out dropdown menu.

---
+++ app/assets/javascripts/top_nav.js
@@ -0,0 +1,13 @@
+// Rotates weightlifter menu icon on click
+
+$(function() {
+  $('#dropdownNavLink').on('click', function() {
+    $('#dropdownNavLink img').addClass('rotate-weightlifter');
+    $('.dropdown-menu').hide().fadeIn(500);
+  });
+
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $('#dropdownNavLink img').toggleClass('rotate-weightlifter');
+    $('.dropdown-menu').fadeOut(250);
+  });
+});
