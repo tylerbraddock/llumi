b4c11b03ca850e557c01c52b0944abb621e60110
Tyler Braddock <tyler.braddock@yahoo.com>
2017-09-12 15:11:10 +0000

    In static_pages/home.html.erb, add homepage content and style in master.scss. Create home.js and animate page scroll on click of learn more button.

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,10 @@
+// Scrolls down page on click
+
+$(function() {
+  $('.jumbotron .btn').on('click', function(e) {
+    e.preventDefault();
+    $('html body').animate({
+      scrollTop: $('#learn-more').offset().top
+    }, 1000);
+  });
+});
